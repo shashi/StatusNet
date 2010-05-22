9c3aed36a8682866adf464ffb0b7523fcc8ebdc2
Shashi Gowda <connect2shashi@gmail.com>
2010-05-22 08:43:54 -0400

    Updated schema

--- db/peopletags.sql
+++ db/peopletags.sql
@@ -1,12 +1,9 @@
 /* this will eventually be merged with statusnet.sql */
 
-/* temporarily keep existing profile_tag data */
-alter table profile_tag rename to profile_tag_old;
-
-/* profile_tag now stores tag metadata. also, remote peopletags are stored here (similar to user_group) */
-create table profile_tag (
-    id integer auto_increment primary key comment 'unique identifier',
-    user_id integer not null comment 'user making the tag' references user (id), /* this may be a remote profile :) */
+/* profile_list stores tag metadata. also, remote peopletags are stored here (similar to user_group) */
+create table profile_list (
+    id integer auto_increment unique key comment 'unique identifier',
+    profile_id integer not null comment 'user making the tag' references profile (id), /* this may be a remote profile :) */
     tag varchar(64) not null comment 'hash tag',
     description text comment 'description for the tag',
 
@@ -16,6 +13,7 @@ create table profile_tag (
     uri varchar(255) unique key comment 'universal identifier',
     mainpage varchar(255) comment 'page for tag info info to link to',
 
+    constraint primary key (tagger, tag),
     index profile_tag_tag_idx (tag)
 ) ENGINE=InnoDB CHARACTER SET utf8 COLLATE utf8_bin;
 
@@ -23,33 +21,11 @@ create table profile_tag (
    description till the user provides a new one */
 /* XXX: uri, modified */
 
-insert into profile_tag (user_id, tag, description)
-    select distinct tagger, tag, tag from profile_tag_old;
-
-/* a table to store tagger - tagged - tag mappings */
-create table profile_tag_map (
-   tagger integer not null comment 'user making the tag' references user (id),  /* this is local */
-   tagged integer not null comment 'profile tagged' references profile (id),    /* this can also be remote */
-   tag integer not null comment 'hash tag' references profile_tag (id),
-   modified timestamp comment 'date the tag was added',
-
-   constraint primary key (tagger, tagged, tag),
-   index profile_tag_modified_idx (modified),
-   index profile_tag_tagger_tag_idx (tagger, tag),
-   index profile_tag_tagged_idx (tagged)
-) ENGINE=InnoDB CHARACTER SET utf8 COLLATE utf8_bin;
-
-/* populate the new profile_tag_map table */
-insert into profile_tag_map (tagger, tagged, tag, modified)
-    select profile_tag_old.tagger, profile_tag_old.tagged, profile_tag.id, profile_tag_old.modified
-        from profile_tag_old join profile_tag
-        on (profile_tag.user_id=profile_tag_old.tagger and profile_tag.tag=profile_tag_old.tag);
-
-drop table profile_tag_old;
+insert into profile_list (user_id, tag, description)
+    select distinct tagger, tag, tag from profile_tag;
 
 create table profile_tag_inbox (
     profile_tag_id integer not null comment 'peopletag receiving the message' references profile_tag (id),
-                /* note: ^ this cannot be a remote peopletag/self-tag (one with a remote uri) */
     notice_id integer not null comment 'notice received' references notice (id),
     created datetime not null comment 'date the notice was created',
 
@@ -61,8 +37,8 @@ create table profile_tag_inbox (
 
 create table profile_tag_subscription (
     profile_tag_id integer not null comment 'foreign key to profile_tag' references profile_tag (id),
-    profile_id integer not null comment 'foreign key to profile table' references profile (id),
 
+    profile_id integer not null comment 'foreign key to profile table' references profile (id),
     created datetime not null comment 'date this record was created',
     modified timestamp comment 'date this record was modified',
 
@@ -72,5 +48,4 @@ create table profile_tag_subscription (
 
 ) ENGINE=InnoDB CHARACTER SET utf8 COLLATE utf8_bin;
 
-/* note: add a profile_tag_id column to ostatus_profile table,
-   this is plugin stuff... goes in Ostatus_profile::schemadef() */
+
