ff5fe767add65fdaddd0e0d75e7be52aa99d43e1
Shashi Gowda <connect2shashi@gmail.com>
2010-07-14 20:05:42 +0000

    Interface for deleting tag and setting it as private. Asks confirmation before proceeding

--- actions/editpeopletag.php
+++ actions/editpeopletag.php
@@ -43,11 +43,14 @@ if (!defined('STATUSNET') && !defined('LACONICA')) {
 class EditpeopletagAction extends OwnerDesignAction
 {
 
-    var $msg;
+    var $msg, $confirm, $confirm_args=array();
 
     function title()
     {
-        return sprintf(_('Edit %s tag'), $this->peopletag->tag);
+        if ($_SERVER['REQUEST_METHOD'] == 'POST' && $this->boolean('delete')) {
+            return sprintf(_('Delete %s people tag'), $this->peopletag->tag);
+        }
+        return sprintf(_('Edit people tag %s'), $this->peopletag->tag);
     }
 
     /**
@@ -57,18 +60,18 @@ class EditpeopletagAction extends OwnerDesignAction
     function prepare($args)
     {
         parent::prepare($args);
-        $this->page = ($this->arg('page')) ? ($this->arg('page')+0) : 1;
-
-        $id = $this->arg('id');
-        $tagger_arg = $this->arg('tagger');
-        $tag_arg = $this->arg('tag');
-        $tagger = common_canonical_nickname($tagger_arg);
-        $tag = common_canonical_tag($tag_arg);
 
         if (!common_logged_in()) {
             $this->clientError(_('Not logged in.'));
             return false;
         }
+
+        $id = $this->arg('id');
+        $tagger_arg = $this->arg('tagger');
+        $tag_arg = $this->arg('tag');
+
+        $tagger = common_canonical_nickname($tagger_arg);
+        $tag = common_canonical_tag($tag_arg);
 
         $current = common_current_user();
 
@@ -76,9 +79,6 @@ class EditpeopletagAction extends OwnerDesignAction
 
         if ($tagger_arg != $tagger || $tag_arg != $tag) {
             $args = array('tagger' => $tagger, 'tag' => $tag);
-            if ($this->page != 1) {
-                $args['page'] = $this->page;
-            }
             common_redirect(common_local_url('editpeopletag', $args), 301);
             return false;
         }
@@ -140,6 +140,41 @@ class EditpeopletagAction extends OwnerDesignAction
         }
     }
 
+    function showConfirm($msg=null, $fwd=null)
+    {
+        $this->confirm = $msg;
+        $this->confirm_args = $fwd;
+        $this->showPage();
+    }
+
+    function showConfirmForm()
+    {
+        $this->elementStart('form', array('id' => 'form_peopletag_edit_confirm',
+                                          'class' => 'form_settings',
+                                          'method' => 'post',
+                                          'action' => common_local_url('editpeopletag',
+                                              array('tagger' => $this->tagger->nickname,
+                                                    'tag' => $this->peopletag->tag))));
+        $this->elementStart('fieldset');
+        $this->hidden('token', common_session_token());
+        $this->hidden('id', $this->arg('id'));
+
+        foreach ($this->confirm_args as $key => $val) {
+            $this->hidden($key, $val);
+        }
+
+        $this->submit('form_action-no',
+                      _m('BUTTON','No'),
+                      'submit form_action-primary',
+                      'cancel');
+        $this->submit('form_action-yes',
+                      _m('BUTTON','Yes'),
+                      'submit form_action-secondary',
+                      'confirm');
+        $this->elementEnd('fieldset');
+        $this->elementEnd('form');
+    }
+
     function showForm($msg=null)
     {
         $this->msg = $msg;
@@ -154,6 +189,11 @@ class EditpeopletagAction extends OwnerDesignAction
 
     function showContent()
     {
+        if ($this->confirm) {
+            $this->showConfirmForm();
+            return;
+        }
+
         $form = new PeopletagEditForm($this, $this->peopletag);
         $form->show();
 
@@ -164,6 +204,8 @@ class EditpeopletagAction extends OwnerDesignAction
     {
         if ($this->msg) {
             $this->element('p', 'error', $this->msg);
+        } else if ($this->confirm) {
+            $this->element('p', 'instructions', $this->confirm);
         } else {
             $this->element('p', 'instructions',
                            _('Use this form to edit the people tag.'));
@@ -178,16 +220,26 @@ class EditpeopletagAction extends OwnerDesignAction
 
     function trySave()
     {
-        $cur = common_current_user();
-        if (empty($cur) || $cur->id != $this->tagger->id) {
-            $this->clientError(_('You must be the creator of the tag to edit it.'), 403);
+        $tag         = common_canonical_tag($this->trimmed('tag'));
+        $description = $this->trimmed('description');
+        $private     = $this->boolean('private');
+        $delete      = $this->arg('delete');
+        $confirm     = $this->arg('confirm');
+        $cancel      = $this->arg('cancel');
+
+        if ($delete && $cancel) {
+            $this->showForm(_('Delete aborted.'));
             return;
         }
 
-        $tag         = common_canonical_tag($this->trimmed('tag'));
-        $description = $this->trimmed('description');
+        $set_private = $private && $this->peopletag->private != $private;
 
-        if (common_valid_tag($tag)) {
+        if ($delete && !$confirm) {
+            $this->showConfirm(_('Deleting this tag will permanantly remove ' .
+                                 'all its subscription and membership records. ' .
+                                 'Do you still want to continue?'), array('delete' => 1));
+            return;
+        } else if (common_valid_tag($tag)) {
             $this->showForm(_('Invalid tag.'));
             return;
         } else if ($tag != $this->peopletag->tag && $this->tagExists($tag)) {
@@ -195,6 +247,15 @@ class EditpeopletagAction extends OwnerDesignAction
             return;
         } else if (Profile_list::descriptionTooLong($description)) {
             $this->showForm(sprintf(_('description is too long (max %d chars).'), Profile_list::maxDescription()));
+            return;
+        } else if ($set_private && !$confirm && !$cancel) {
+            $fwd = array('tag' => $tag,
+                         'description' => $description,
+                         'private' => (int) $private);
+
+            $this->showConfirm(_('Setting a public tag as private will ' .
+                                 'permanently remove all the existing ' .
+                                 'subscriptions to it. Do you still want to continue?'), $fwd);
             return;
         }
 
@@ -204,6 +265,9 @@ class EditpeopletagAction extends OwnerDesignAction
 
         $this->peopletag->tag         = $tag;
         $this->peopletag->description = $description;
+        if (!$set_private || $confirm) {
+            $this->peopletag->private     = $private;
+        }
 
         $result = $this->peopletag->update($orig);
 
@@ -213,6 +277,19 @@ class EditpeopletagAction extends OwnerDesignAction
         }
 
         $this->peopletag->query('COMMIT');
+
+        if ($set_private && $confirm) {
+            Profile_tag_subscription::cleanup($this->peopletag);
+        }
+
+        if ($delete) {
+            // This might take quite a bit of time.
+            $this->peopletag->delete();
+            // send home.
+            common_redirect(common_local_url('all',
+                                         array('nickname' => $this->tagger->nickname)),
+                            303);
+        }
 
         if ($tag != $orig->tag) {
             common_redirect(common_local_url('editpeopletag',
@@ -229,9 +306,6 @@ class EditpeopletagAction extends OwnerDesignAction
         $args = array('tagger' => $this->tagger->id, 'tag' => $tag);
         $ptag = Profile_list::pkeyGet($args);
 
-        if (!empty($ptag)) {
-            return true;
-        }
-        return false;
+        return !empty($ptag);
     }
 }
