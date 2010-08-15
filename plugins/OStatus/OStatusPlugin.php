d1e49c7fbca90fdfe0a3d139dc3bf7b804023579
Shashi Gowda <connect2shashi@gmail.com>
2010-08-15 17:06:29 +0000

    Add a remote tag button against every profile (when not logged in)

--- plugins/OStatus/OStatusPlugin.php
+++ plugins/OStatus/OStatusPlugin.php
@@ -58,6 +58,10 @@ class OStatusPlugin extends Plugin
                     array('action' => 'ownerxrd'));
         $m->connect('main/ostatus',
                     array('action' => 'ostatusinit'));
+        $m->connect('main/ostatustag',
+                    array('action' => 'ostatustag'));
+        $m->connect('main/ostatustag?nickname=:nickname',
+                    array('action' => 'ostatustag'), array('nickname' => '[A-Za-z0-9_-]+'));
         $m->connect('main/ostatus?nickname=:nickname',
                   array('action' => 'ostatusinit'), array('nickname' => '[A-Za-z0-9_-]+'));
         $m->connect('main/ostatus?group=:group',
@@ -229,20 +233,7 @@ class OStatusPlugin extends Plugin
      */
     function onStartProfileRemoteSubscribe($output, $profile)
     {
-        $cur = common_current_user();
-
-        if (empty($cur)) {
-            // Add an OStatus subscribe
-            $output->elementStart('li', 'entity_subscribe');
-            $url = common_local_url('ostatusinit',
-                                    array('nickname' => $profile->nickname));
-            $output->element('a', array('href' => $url,
-                                        'class' => 'entity_remote_subscribe'),
-                                _m('Subscribe'));
-
-            $output->elementEnd('li');
-        }
-
+        $this->onStartProfileListItemActionElements($output, $profile);
         return false;
     }
 
@@ -308,7 +299,7 @@ class OStatusPlugin extends Plugin
         $action->elementEnd('form');
     }
 
-    function onStartSavePeopletags($action, $profile)
+    function onStartTagProfileAction($action, $profile)
     {
         $err = null;
         $uri = $action->trimmed('uri');
@@ -1185,7 +1176,7 @@ class OStatusPlugin extends Plugin
         return true;
     }
 
-    function onStartProfileListItemActionElements($item)
+    function onStartProfileListItemActionElements($item, $profile=null)
     {
         if (!common_logged_in()) {
 
@@ -1193,7 +1184,12 @@ class OStatusPlugin extends Plugin
 
             if (!empty($profileUser)) {
 
-                $output = $item->out;
+                if ($item instanceof Action) {
+                    $output = $item;
+                    $profile = $item->profile;
+                } else {
+                    $output = $item->out;
+                }
 
                 // Add an OStatus subscribe
                 $output->elementStart('li', 'entity_subscribe');
@@ -1202,6 +1198,14 @@ class OStatusPlugin extends Plugin
                 $output->element('a', array('href' => $url,
                                             'class' => 'entity_remote_subscribe'),
                                  _m('Subscribe'));
+                $output->elementEnd('li');
+
+                $output->elementStart('li', 'entity_tag');
+                $url = common_local_url('ostatustag',
+                                        array('nickname' => $profileUser->nickname));
+                $output->element('a', array('href' => $url,
+                                            'class' => 'entity_remote_tag'),
+                                 _m('Tag'));
                 $output->elementEnd('li');
             }
         }
